ea9202d9cbce0a30a71861b969d80c019816efb1
Basset <aldana.sorni@gmail.com>
2022-01-20 21:37:41 -0300

    hice más claro mi código y corregí muchos errores

--- jerbito2.js
+++ jerbito2.js
@@ -1,10 +1,10 @@
-const inputEmail = document.querySelector('#input-email')
-const inputPass = document.querySelector('#input-pass')
-const inputRepeatPass = document.querySelector('#input-repeat-pass')
-const inputCheck = document.querySelector('#input-check')
-const inputSubmit = document.querySelector('#input-submit')
+const inputEmail = document.getElementById('input-email');
+const inputPass = document.getElementById('input-pass');
+const inputRepeatPass = document.getElementById('input-repeat-pass');
+const inputCheck = document.getElementById('input-check');
+const inputSubmit = document.getElementById('input-submit');
 
-let email, password, repeatPassword
+let email, password, repeatPassword;
 
 const guardarDatos = () => {
     email = inputEmail.value
@@ -12,33 +12,33 @@ const guardarDatos = () => {
     repeatPassword = inputRepeatPass.value
 }
 
-const validar = document.querySelector('.mostrarCartelOk'); //Las contraseñas coinciden
-const alerta = document.querySelector('.mostrarCartelError'); //Las contraseñas no coinciden
-const exito = document.querySelector('.mostrarCartelExito'); //Se registró
-const alerta2 = document.querySelector('.mostrarCartelError2'); //No se registró
+
+const notificacionRegistro = document.getElementById('notificacion-registro');
 
  const validatePass = () => {
      if (inputPass.value === inputRepeatPass.value) {
-         validar.classList.remove('cartelOculto');
-         alerta.classList.add('cartelOculto');
-     } else {
-         alerta.classList.remove('cartelOculto');
-         validar.classList.add('cartelOculto');
-         inputSubmit.setAttribute('disabled', 'disabled');
-     }
-}
 
-const registroExitoso = () => {
-    if ((inputCheck.checked === true) && (email != '') && (password != '') && (repeatPassword != '') ) {
-        exito.classList.remove('cartelOculto');
-        validar.classList.add('cartelOculto');
+         notificacionRegistro.style.color = 'green'
+         notificacionRegistro.innerText = 'Las contraseñas coinciden!'
+         inputSubmit.removeAttribute('disabled', 'disabled');
+        } else {
+            notificacionRegistro.style.color = 'red'
+            notificacionRegistro.innerText = 'Las contraseñas no coinciden!'   
+        }
+    }
+    
+    const registroExitoso = () => {
+        if ((inputCheck.checked === true) && (email != '') && (password != '') && (repeatPassword != '') ) {
+            notificacionRegistro.style.color = 'green'
+            notificacionRegistro.innerText = 'Te registraste exitosamente!'
     } else if (inputCheck.checked === false || email === '' || password === '' || repeatPassword === '') {
-        alerta2.classList.remove('cartelOculto');
-        exito.classList.add('cartelOculto');
-        validar.classList.add('cartelOculto');
+        notificacionRegistro.style.color = 'red'
+        notificacionRegistro.innerText = 'Por favor, completá todos los campos!'
         inputSubmit.setAttribute('disabled', 'disabled');
     }
 }
+registroExitoso();
+
 
 inputSubmit.addEventListener('click', (e) => {
     e.preventDefault();
@@ -46,5 +46,5 @@ inputSubmit.addEventListener('click', (e) => {
     registroExitoso();
 })
 
-inputRepeatPass.addEventListener('change', validatePass) 
-inputRepeatPass.addEventListener('change', validatePass) 
+inputRepeatPass.addEventListener('change', validatePass); 
+
